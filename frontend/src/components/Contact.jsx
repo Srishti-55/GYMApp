4f7c4d02a4715180d24687cf287a3e29751a56e1
Srishti Chanda <Srishti.Chanda@walmart.com>
2024-10-27 22:44:00 +0530

    fixed contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,79 +1,85 @@
-import React, { useState } from 'react'
-import { ClipLoader } from 'react-spinners'
+import axios from "axios";
+import React, { useState } from "react";
+import { ClipLoader } from "react-spinners";
+import { toast } from "react-toastify";
 
 const Contact = () => {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [message, setMessage] = useState("")
-  const [loading, setLoading] = useState(false)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const sendMail = async(e) =>{
+  const sendMail = async (e) => {
     e.preventDefault();
     setLoading(true);
-    try{
-      const {data} = await axios.post(
-        "http://localhost:4000/send/mail", 
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/send/mail",
         {
-          name, 
-          email, 
-          message
+          name,
+          email,
+          message,
         },
         {
           withCredentials: true,
-          headers: {"Content-Type": "application/json"},
+          headers: { "Content-Type": "application/json" },
         }
-      )
+      );
       setName("");
       setEmail("");
       setMessage("");
       toast.success(data.message);
       setLoading(false);
     } catch (error) {
-      setLoading(false)
-      toast.error(error.response.data.message)
+      setLoading(false);
+      toast.error(error.response.data.message);
     }
+  };
 
-  }
   return (
-    <section className='contact'>
+    <section className="contact">
       <form onSubmit={sendMail}>
-        <h1>Contact Us</h1>
+        <h1>CONTACT US</h1>
         <div>
           <label>Name</label>
-          <input type="text"
+          <input
+            type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
           <label>Email</label>
-          <input type="=email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label>Message</label>
-          <input type="text" 
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
-        <button 
-           type="submit" 
-           disabled={loading}
-           style={{
+        <button
+          type="submit"
+          disabled={loading}
+          style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            gap: "15px"
-           }}>
-          {loading && <ClipLoader size={20} color="white"/>}
+            gap: "15px",
+          }}
+        >
+          {loading && <ClipLoader size={20} color="white" />}
           Send Message
         </button>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
