062f736dbcfdd064715165796593c30f2450325e
Srishti Chanda <Srishti.Chanda@walmart.com>
2024-10-27 22:38:32 +0530

    added email to fronted and fixed localhost endpoint

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -6,9 +6,37 @@ const Contact = () => {
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
   const [loading, setLoading] = useState(false)
+
+  const sendMail = async(e) =>{
+    e.preventDefault();
+    setLoading(true);
+    try{
+      const {data} = await axios.post(
+        "http://localhost:4000/send/mail", 
+        {
+          name, 
+          email, 
+          message
+        },
+        {
+          withCredentials: true,
+          headers: {"Content-Type": "application/json"},
+        }
+      )
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success(data.message);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false)
+      toast.error(error.response.data.message)
+    }
+
+  }
   return (
     <section className='contact'>
-      <form>
+      <form onSubmit={sendMail}>
         <h1>Contact Us</h1>
         <div>
           <label>Name</label>
