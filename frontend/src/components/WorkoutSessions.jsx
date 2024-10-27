020f901fa679c8ab48542b0ccacc3cbcccb719b6
Srishti Chanda <Srishti.Chanda@walmart.com>
2024-10-27 18:38:09 +0530

    navbar,her0,workoutSession added

--- frontend/src/components/WorkoutSessions.jsx
+++ frontend/src/components/WorkoutSessions.jsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 const WorkoutSessions = () => {
   return (
-    <div>WorkoutSessions</div>
+    <section className='workout_session'>
+      <div className='wrapper'>
+        <h1>TOP WORKOUT SESSIONS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit aut perspiciatis nostrum numquam ipsa maxime? Nihil, cumque quisquam.
+        </p>
+        <img src="/img5.jpg" alt="workout"/>
+
+      </div>
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione velit sapiente laborum architecto ab illo perspiciatis atque molestias?
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, aliquam itaque ipsa molestias ab laboriosam?</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, aliquam itaque ipsa molestias ab laboriosam?</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, aliquam itaque ipsa molestias ab laboriosam?</p>
+          </div>
+          <div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, aliquam itaque ipsa molestias ab laboriosam?</p>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
